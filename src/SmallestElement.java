b3a3aef3f074dee77d1454dca9a5820aebac67ee
Tin Phan <tinphan09@gmail.com>
2018-05-05 16:16:24 -0500

    finished big, small, and total

--- src/SmallestElement.java
+++ src/SmallestElement.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class SmallestElement {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input a value: ");
+        int x = scanner.nextInt();
+
+        int[] a = new int[x];
+        for(int i = 0; i < x; i++){
+            a[i] = (int) (Math.random() * 100);
+            System.out.print(a[i] + "  ");
+        }
+        int small = a[0];
+        for(int i = 1; i < a.length; i++) {
+            if (small > a[i]) {
+                small = a[i];
+            }
+        }
+        System.out.println();
+        System.out.println("The smallest number is " + small + ".");
+    }
 }
--- src/TotalArray.java
+++ src/TotalArray.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class TotalArray {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input a value: ");
+
+        int x  = scanner.nextInt();
+        int[] a = new int[100];
+        for(int i = 0; i < x; i++){
+             a[i] = (int) (Math.random() * 100);
+             System.out.print(a[i] + " ");
+        }
+        int total = 0;
+        for(int i = 0; i < x; i++){
+            total = total + a[i];
+        }
+        System.out.println();
+        System.out.println("The total value of the array is " + total + ".");
+    }
 }
