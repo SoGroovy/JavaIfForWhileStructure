2fef8f598c02ffe7e4f1c94c5029c6de26cccd32
Tin Phan <tinphan09@gmail.com>
2018-05-05 16:26:14 -0500

    big,small,total,avg

--- src/AvgArray.java
+++ src/AvgArray.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+
 public class AvgArray {
-    public
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input a value: ");
+        int x = scanner.nextInt();
+
+        int[] a = new int[x];
+        for(int i = 0; i < x; i++){
+            a[i] = (int) (Math.random() * 100);
+            System.out.print(a[i] + " ");
+        }
+        int total = 0;
+        for(int i = 0; i < x; i++){
+            total = total + a[i];
+        }
+        double average = ((double) total)/x;
+        System.out.println();
+        System.out.println("The average of the array is " + average + ".");
+    }
 }
