f7e5282789f3ed577a6ecb281f581ec368a8ee9f
Tin Phan <tinphan09@gmail.com>
2018-05-06 15:05:56 -0500

    qwe

--- src/SelectionSortArray.java
+++ src/SelectionSortArray.java
@@ -13,16 +13,17 @@ public class SelectionSortArray {
         }
         display(a);
         for (int j = size - 1; j > 0; j--) {
-            int index = j;
-            for (int i = 0; i < size - 1 ; i++) {
-                if (a[i] > a[index]) {
-                    int biggerNumber = a[i];
-                    a[i] = a[index];
-                    a[index] = biggerNumber;
+            int bigPos = j;
+            for (int i = 0; i < a.length; i++){
+                if (a[bigPos] < a[i]){
+                    bigPos = i;
+                    int last = a[j];
+                    a[j] = a[bigPos];
+                    a[bigPos] = last;
                 }
             }
-            display(a);
         }
+        display(a);
 //        int bigPos = 0;
 //
 //        for (int i = 1; i < a.length; i++) {
@@ -31,19 +32,6 @@ public class SelectionSortArray {
 //            }
 //        }
 //        System.out.println("The index with the biggest value is: " + bigPos);
-        int bigPos = 0;
-
-        for (int i = 1; i < a.length; i++) {
-            if (a[bigPos] < a[i]) {
-                bigPos = i;
-            }
-        }
-        for (int j = size - 1; j > 0; j--){
-            int last = a[bigPos];
-            a[bigPos] = a[j];
-            a[j] = last;
-        }
-        display(a);
     }
 
     public static void display(int[] a) {
