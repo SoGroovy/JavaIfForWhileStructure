a8f0ef00bb461406fc00c6f25d87aa23090ddffd
Tin Phan <tinphan09@gmail.com>
2018-05-06 17:53:50 -0500

    m

--- src/EvenVSOdd.java
+++ src/EvenVSOdd.java
@@ -1,34 +1,68 @@
 import java.util.Scanner;
 
 public class EvenVSOdd {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Input a value: ");
         int size = scanner.nextInt();
 
         int[] a = new int[size];
-        for (int i = 0; i < size; i++){
-            a[i] = (int)(Math.random() * 100);
+        for (int i = 0; i < size; i++) {
+            a[i] = (int) (Math.random() * 100);
 
         }
         display(a);
-        System.out.println(max(a));
-
+        System.out.println("The biggest number within the array is: " + max(a));
+        System.out.println("The total amount of even numbers is: " + totalEvenNumber(a));
+        System.out.println("The sum of the even numbers is: " + sumEvenNumber(a));
+        System.out.println("The sum of th even numbers with the while loop is: " + sumEWhile(a));
     }
-    public static void display(int [] a){
+
+    public static void display(int[] a) {
         System.out.println();
-        for (int i = 0; i < a.length; i++){
+        for (int i = 0; i < a.length; i++) {
             System.out.print(a[i] + " ");
         }
     }
-    public static int max(int[] a){
+
+    public static int max(int[] a) {
         System.out.println();
         int m = a[0];
-        for (int i = 1; i < a.length; i++){
-            if(m < a[i]){
+        for (int i = 1; i < a.length; i++) {
+            if (m < a[i]) {
                 m = a[i];
             }
         }
         return m;
+    }
+
+    public static int totalEvenNumber(int[] a) {
+        int count = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] % 2 == 0) {
+                count = +1;
+            }
+        }
+        return count;
+    }
+    public static int sumEvenNumber(int[] a) {
+        int sum = 0;
+        for (int i = 0; i < a.length; i++){
+            if (a[i] % 2 == 0){
+                sum = sum + a[i];
+            }
+        }
+        return sum;
+    }
+    public static int sumEWhile(int[] a){
+        int sum = 0;
+        int i = 0;
+        while(i < a.length){
+            if(a[i] % 2 == 0){
+                sum = sum + a[i];
+            }
+            i++;
+        }
+        return sum;
     }
 }
--- src/SelectionSortArray.java
+++ src/SelectionSortArray.java
@@ -11,19 +11,23 @@ public class SelectionSortArray {
             a[i] = (int) (Math.random() * 100);
             System.out.print(a[i] + " ");
         }
-        display(a);
-        for (int workingSize = size; workingSize > 0; workingSize-- ) {
-            int bigPos = 0;
-            for (int i = 1; i < a.length; i++) {
-                if (a[bigPos] < a[i]) {
-                    bigPos = i;
-                }
-            }
-            int last = a[bigPos];
-            a[bigPos] = a[size - 1];
-            a[size - 1] = last;
+        for(int j = size; j > 0; j--) {
+            swapLarge(a, j);
         }
         display(a);
+//        display(a);
+//        for (int workingSize = size; workingSize > 0; workingSize-- ) {
+//            int bigPos = 0;
+//            for (int i = 1; i < a.length; i++) {
+//                if (a[bigPos] < a[i]) {
+//                    bigPos = i;
+//                }
+//            }
+//            int last = a[bigPos];
+//            a[bigPos] = a[size - 1];
+//            a[size - 1] = last;
+//        }
+//        display(a);
 
     }
 
@@ -32,5 +36,17 @@ public class SelectionSortArray {
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i] + " ");
         }
+    }
+    public static void swapLarge(int[] a, int b){
+        int swap;
+        int indexOfMax = 0;
+        for (int i = 1; i < b; i++){
+            if(a[indexOfMax] < a[i]){
+                indexOfMax = i;
+            }
+        }
+        swap = a[indexOfMax];
+        a[indexOfMax] = a[b-1];
+        a[b-1] = swap;
     }
 }
