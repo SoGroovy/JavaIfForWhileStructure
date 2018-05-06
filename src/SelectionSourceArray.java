6396f6b5120c19929f7d3e8d1a500e1cf1176120
Tin Phan <tinphan09@gmail.com>
2018-05-06 12:15:35 -0500

    check Selection Source

--- src/SelectionSourceArray.java
+++ src/SelectionSourceArray.java
@@ -1,2 +1,34 @@
+import java.util.Scanner;
+
 public class SelectionSourceArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input a value: ");
+        int size = scanner.nextInt();
+
+        int[] a = new int[size];
+        for (int i = 0; i < size; i++) {
+            a[i] = (int) (Math.random() * 100);
+            System.out.print(a[i] + " ");
+        }
+        display(a);
+        for (int j = size - 1; j > 0; j--) {
+            int index = j;
+            for (int i = 0; i < size - 1 ; i++) {
+                if (a[i] > a[index]) {
+                    int biggerNumber = a[i];
+                    a[i] = a[index];
+                    a[index] = biggerNumber;
+                }
+            }
+            display(a);
+        }
+    }
+
+    public static void display(int[] a) {
+        System.out.println();
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
 }
