37cc6491db1cbd3da44c49626dd982a02c0094d1
Tin Phan <tinphan09@gmail.com>
2018-05-07 17:49:43 -0500

    m

--- src/SecondBiggestInt.java
+++ src/SecondBiggestInt.java
@@ -6,22 +6,21 @@ public class SecondBiggestInt {
 //        System.out.println("Input value here: ");
 //        int size = scanner.nextInt();
 
-        int[] a = new int[6];
-        int start = -1;
-        for (int i = 0; i < 6; i++) {
-            a[i] = start;
-            System.out.print(a[i] + " ");
-            start--;
-        }
+        int[] a = new int[]{9, 9, 9, 8, 1, 2, 0, 1};
+//        int start = -1;
+//        for (int i = 0; i < 6; i++) {
+//            a[i] = start;
+//            System.out.print(a[i] + " ");
+//            start--;
+//        }
         System.out.println();
         System.out.println("The second largest number in the array is: " + secondLargest(a));
     }
 
     public static int secondLargest(int[] a) {
-            int max = a[0];
-            int secondMax =a[1];
-            for (int i = 1; i < a.length; i++) {
-                if (a[i] > max)
+            int max = Integer.MIN_VALUE;
+            int secondMax = Integer.MIN_VALUE;
+            for (int i = 0; i < a.length; i++) {
                 if (max < a[i]) {
                     secondMax = max;
                     max = a[i];
